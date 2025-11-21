c9441c3db06bbfbc2d5d816f89baa9b8bc4432ee
Kazu Yamamoto <kazu@iij.ad.jp>
2025-11-21 09:13:32 +0900

    using http2 v5.4

--- Network/HTTP2/TLS/Config.hs
+++ Network/HTTP2/TLS/Config.hs
@@ -5,10 +5,8 @@ module Network.HTTP2.TLS.Config where
 
 import Data.ByteString (ByteString)
 import Foreign.Marshal.Alloc (free, mallocBytes)
-import Network.HTTP2.Client (
-    Config (..),
-    defaultPositionReadMaker,
- )
+import Network.HTTP2.Client (Config)
+import qualified Network.HTTP2.Client as C
 import Network.Socket (SockAddr)
 import Network.Socket.BufferPool
 import qualified System.TimeManager as T
@@ -27,22 +25,22 @@ allocConfigForServer Settings{..} mgr send recv mysa peersa = do
     buf <- mallocBytes settingsSendBufferSize
     recvN <- makeRecvN "" recv
     let config =
-            Config
-                { confWriteBuffer = buf
-                , confBufferSize = settingsSendBufferSize
-                , confSendAll = send
-                , confReadN = recvN
-                , confPositionReadMaker = defaultPositionReadMaker
-                , confTimeoutManager = mgr
-                , confMySockAddr = mysa
-                , confPeerSockAddr = peersa
-                , confReadNTimeout = False
+            C.defaultConfig
+                { C.confWriteBuffer = buf
+                , C.confBufferSize = settingsSendBufferSize
+                , C.confSendAll = send
+                , C.confReadN = recvN
+                , C.confPositionReadMaker = C.defaultPositionReadMaker
+                , C.confTimeoutManager = mgr
+                , C.confMySockAddr = mysa
+                , C.confPeerSockAddr = peersa
+                , C.confReadNTimeout = False
                 }
     return config
 
 -- | Deallocating the resource of the simple configuration.
 freeConfigForServer :: Config -> IO ()
-freeConfigForServer conf = free $ confWriteBuffer conf
+freeConfigForServer conf = free $ C.confWriteBuffer conf
 
 allocConfigForClient
     :: (ByteString -> IO ()) -> IO ByteString -> SockAddr -> SockAddr -> IO Config
@@ -54,21 +52,21 @@ allocConfigForClient send recv mysa peersa = do
     -- So, a timeout manager is created per connection.
     mgr <- T.initialize 30000000 -- fixme
     let config =
-            Config
-                { confWriteBuffer = buf
-                , confBufferSize = wbufsiz
-                , confSendAll = send
-                , confReadN = recvN
-                , confPositionReadMaker = defaultPositionReadMaker
-                , confTimeoutManager = mgr
-                , confMySockAddr = mysa
-                , confPeerSockAddr = peersa
-                , confReadNTimeout = False
+            C.defaultConfig
+                { C.confWriteBuffer = buf
+                , C.confBufferSize = wbufsiz
+                , C.confSendAll = send
+                , C.confReadN = recvN
+                , C.confPositionReadMaker = C.defaultPositionReadMaker
+                , C.confTimeoutManager = mgr
+                , C.confMySockAddr = mysa
+                , C.confPeerSockAddr = peersa
+                , C.confReadNTimeout = False
                 }
     return config
 
 -- | Deallocating the resource of the simple configuration.
 freeConfigForClient :: Config -> IO ()
-freeConfigForClient Config{..} = do
-    free confWriteBuffer
-    T.killManager confTimeoutManager
+freeConfigForClient conf = do
+    free $ C.confWriteBuffer conf
+    T.killManager $ C.confTimeoutManager conf
